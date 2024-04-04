abf39036b9a54f6a06b8937c864c36ca7bf95729
Ramkumar S <ramkumarcse07@gmail.com>
2024-04-04 07:56:36 +0530

    Foodies App

---
+++ app/meals/share/error.js
@@ -0,0 +1,10 @@
+"use client";
+const Error = ({ error }) => {
+  return (
+    <main className="error">
+      <h1>An Error Occurred...</h1>
+      <p>Failed to create a meal</p>
+    </main>
+  );
+};
+export default Error;
---
+++ components/main-header/main-header.js
@@ -0,0 +1,31 @@
+import Image from "next/image";
+import logoImg from "@/assets/logo.png";
+import Link from "next/link";
+import classes from "./main-header.module.css";
+import MainHeaderBackground from "./main-header-background";
+import NavLink from "./nav-link";
+
+const MainHeader = () => {
+  return (
+    <>
+      <MainHeaderBackground />
+      <header className={classes.header}>
+        <Link className={classes.logo} href="/">
+          <Image src={logoImg} alt="Header Logo" priority />
+          Next Level Food
+        </Link>
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <NavLink href="/meals">Browse Meals</NavLink>
+            </li>
+            <li>
+              <NavLink href="/community">Foodies Community</NavLink>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+};
+export default MainHeader;
